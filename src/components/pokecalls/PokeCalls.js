c62839e94a7de2f5f29992cf8a7fd0576f4aa75a
lwatson2 <watson.logan@yahoo.com>
2019-08-27 16:03:40 -0500

    Testing fix for pagination

--- src/components/pokecalls/PokeCalls.js
+++ src/components/pokecalls/PokeCalls.js
@@ -125,8 +125,7 @@ class PokeCalls extends Component {
       currentPageNum = 1;
     }
     currentUrlParams.set("page", currentPageNum);
-    console.log(window.location.pathname);
-    this.props.history.push(`?${currentUrlParams}`);
+    this.props.history.push(`${window.location.pathname}?${currentUrlParams}`);
     this.setState(
       {
         pokemonList: [],
